d9b0e418ff46dcfc5be1e34c247d9b764fe7576e
f472918106 <472918106@qq.com>
2018-04-03 10:50:00 +0800

    初始化

---
+++ BasedOnSocket/SimpleWebServer/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleWebServer
+{
+    class Program
+    {
+        /// <summary>
+        /// SimpleWebServer
+        /// 向请求的浏览器返回一个静态HTML页面
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //获取本机IP地址,即127.0.0.1
+            IPAddress localaddress = IPAddress.Loopback;
+
+            //创建可以访问的断点,49155表示端口号,如果此时端口号设置为零,系统会默认分配一个空闲端口号
+            IPEndPoint endpoint = new IPEndPoint(localaddress, 65000);
+
+            //创建Socket对象,使用IPv4地址,数据通信类型为数据流,传输控制协议为TCP
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //将Socket绑定到断点上
+            socket.Bind(endpoint);
+            //设置连接队列长度
+            socket.Listen(10);
+
+            while(true)
+            {
+                Console.WriteLine("[" + DateTime.Now + "] Wait an connect Request...");
+                //开始监听,阻塞程序线程的执行,直到接收到一个客户端的连接请求
+                Socket clientsocket = socket.Accept();
+
+                //输出客户端地址
+                Console.WriteLine("Client Address is {0}", clientsocket.RemoteEndPoint);
+                //把客户端请求数据读入保存到一个数组中
+                byte[] buffer = new byte[2048];
+
+                int receivelength = clientsocket.Receive(buffer, 2048, SocketFlags.None);
+                string requeststring = Encoding.UTF8.GetString(buffer, 0, receivelength);
+
+                //在客户端输出请求的消息
+                Console.WriteLine(requeststring);
+
+                //服务器做出相应内容
+                //响应的状态行
+                string statusLine = "HTTP/1.1 200 OK\r\n";
+                byte[] responseStatusLineBytes = Encoding.UTF8.GetBytes(statusLine);
+                string responseBody = "<html><head><title>Hello World</title></head><body><b>Web Server Running...</b></body></html";
+                string responseHeader = string.Format("Content-Type: text/html; charest=UTF-8\r\nContent-Length: {0}\r\n", responseBody.Length);
+                byte[] responseHeaderBytes = Encoding.UTF8.GetBytes(responseHeader);
+                byte[] responseBodyBytes = Encoding.UTF8.GetBytes(responseBody);
+
+                //向客户端发送状态行
+                clientsocket.Send(responseStatusLineBytes);
+
+                //向客户端发送回应头信息
+                clientsocket.Send(responseHeaderBytes);
+
+                //向客户端发送头部和内容的空行
+                clientsocket.Send(new byte[] { 13, 10 });
+
+                //向客户端发送主体部分
+                clientsocket.Send(responseBodyBytes);
+
+                //断开连接
+                clientsocket.Close();
+                Console.ReadKey();
+                break;
+            }
+
+            //关闭服务器
+            socket.Close();
+        }
+    }
+}
---
+++ BasedOnTcpListener/SimpleWebServer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace SimpleWebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //获取本机IP地址,即127.0.0.1
+            IPAddress Localaddress = IPAddress.Loopback;
+
+            //创建可以访问的断点
+            IPEndPoint endpoint = new IPEndPoint(Localaddress, 65000);
+
+            //创建Tcp监听器
+            TcpListener tcplistener = new TcpListener(endpoint);
+
+            //启动监听
+            tcplistener.Start();
+            Console.WriteLine("[" + DateTime.Now + "]" + "Wait an connect Request...");
+            while(true)
+            {
+                TcpClient client = tcplistener.AcceptTcpClient();
+                if(client.Connected == true)
+                {
+                    Console.WriteLine("[" + DateTime.Now + "]" + "Created connection");
+                }
+
+                //获得一个网络流对象
+                //该网络流对象封装了Socket的输入和输出操作
+                //此时通过对网络流对象进行写入来返回响应消息
+                //通过对网络流对象进行读取来获得请求消息
+                NetworkStream netstream = client.GetStream();
+                //把客户端请求读入保存到一个数组中
+                byte[] buffer = new byte[2048];
+
+                int receivelength = netstream.Read(buffer, 0, 2048);
+                string requeststring = Encoding.UTF8.GetString(buffer, 0, receivelength);
+
+                //在服务器输出请求消息
+                Console.WriteLine(requeststring);
+
+                //服务器做出响应内容
+                //响应的状态行
+                string statusLine = "HTTP/1.1 200 OK\r\n";
+                byte[] responseStatusLineBytes = Encoding.UTF8.GetBytes(statusLine);
+                string responseBody = "<html><head><title>Hello World</title></head><body><b>Web Server Running...</b></body></html";
+                string responseHeader = string.Format("Content-Type: text/html; charest=UTF-8\r\nContent-Length: {0}\r\n", responseBody.Length);
+                byte[] responseHeaderBytes = Encoding.UTF8.GetBytes(responseHeader);
+                byte[] responseBodyBytes = Encoding.UTF8.GetBytes(responseBody);
+
+                //写入状态行信息
+                netstream.Write(responseStatusLineBytes, 0, responseStatusLineBytes.Length);
+                //写入回应的头部
+                netstream.Write(responseHeaderBytes, 0, responseHeaderBytes.Length);
+                //写入回应头部和内容之间的空行
+                netstream.Write(new byte[] { 13, 10 }, 0, 2);
+
+                //写入回应的内容
+                netstream.Write(responseBodyBytes, 0, responseBodyBytes.Length);
+
+                //关闭客户端连接
+                client.Close();
+                Console.ReadKey();
+                break;
+            }
+
+            //关闭服务器
+            tcplistener.Stop();
+        }
+    }
+}
